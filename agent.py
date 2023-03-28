fd9869ae64d5573bf10a3bad8cdc99cfd83d8d82
abecoup <abe.coup5@gmail.com>
2023-03-28 18:51:22 -0400

    integrated alpaca api

--- agent.py
+++ agent.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-
-
+from config import *
+from alpaca.trading.client import TradingClient
 
 def main():
-    print("test")
-
-
+    trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
+    account = trading_client.get_account()
+    for property_name, value in account:
+        print(f"\"{property_name}\": {value}")
 
 
 if __name__ == '__main__':
